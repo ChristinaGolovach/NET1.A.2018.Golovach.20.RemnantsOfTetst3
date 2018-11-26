4d16785fa7c4a773487fa795d7a675c7b6facfa9
Christina <christina.golovach@gmail.com>
2018-11-26 20:42:52 +0300

    Add task 6  solution

--- No6.Solution.Tests/CustomEnumerableTests.cs
+++ No6.Solution.Tests/CustomEnumerableTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using No6;
+using System.Linq;
+using NUnit.Framework;
 
 namespace No6.Solution.Tests
 {
@@ -8,25 +10,36 @@ namespace No6.Solution.Tests
         [Test]
         public void Generator_ForSequence1()
         {
+            // Act
+            int[] result =  Generator.GenerateSequence<int>(10, (x1, x2) =>  x1 + x2 , 1, 1).ToArray();
+
             int[] expected = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
 
-            Assert.Inconclusive();
+            // Assert
+            CollectionAssert.AreEqual(expected, result);
         }
 
         [Test]
         public void Generator_ForSequence2()
         {
+            int[] result = Generator.GenerateSequence<int>(10, (x1, x2) => 6 * x2 - 8 * x1, 1, 2).ToArray();
+
             int[] expected = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
 
-            Assert.Inconclusive();
+            CollectionAssert.AreEqual(expected, result);
         }
 
         [Test]
         public void Generator_ForSequence3()
         {
+            double[] result = Generator.GenerateSequence<double>(10, (x1, x2) => x2 + x1 / x2, 1, 2).ToArray();
+
             double[] expected = { 1, 2, 2.5, 3.3, 4.05757575757576, 4.87086926018965, 5.70389834408211, 6.55785277425587, 7.42763417076325, 8.31053343902137 };
 
-            Assert.Inconclusive();
+            for ( int i = 0; i < result.Length; i++)
+            {
+                Assert.AreEqual(result[i], expected[i], 0.0000001);
+            }
         }
     }
 }
---
+++ No6/Generator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No6
+{
+    public static class Generator
+    {
+        public static IEnumerable<T> GenerateSequence<T>(int count, Func<T, T, T> ruleGeneration, T firstItem, T secondItem)
+        {
+            if (ruleGeneration == null)
+            {
+                throw new ArgumentNullException($"The {nameof(ruleGeneration)} can not be null");
+            }
+
+            if (count < 2)
+            {
+                throw new ArgumentException($"The {nameof(count)} must be more than one.");
+            }
+
+            if (count == 2)
+            {
+                return new T[] { firstItem, secondItem };
+            }
+
+            return GenerateSequenceCore(count, ruleGeneration, firstItem, secondItem);
+        }
+
+        private static IEnumerable<T> GenerateSequenceCore<T>(int count, Func<T, T, T> ruleGeneration, T firstItem, T secondItem)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return firstItem;
+
+                T temp = secondItem;
+                secondItem = ruleGeneration(firstItem, secondItem);
+                firstItem = temp;
+            }
+        }
+    }
+}
